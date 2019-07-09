67a5dba8129a80359d8cf01db3b425b0662aa1ca
jfcameron <jf.cameron@gmail.com>
2019-07-09 19:58:54 -0400

    wip

---
+++ test/window_test.cpp
@@ -0,0 +1,24 @@
+// © 2019 Joseph Cameron - All Rights Reserved
+
+#include <string>
+
+#include <jfc/catch.hpp>
+#include <jfc/types.h>
+
+#include <gdk/window.h>
+
+using namespace gdk;
+
+TEST_CASE("window constructors", "[window]" )
+{
+    SECTION("blar")
+    {
+        constexpr auto name = "Cool Window";
+
+        SimpleGLFWWindow window(name);      
+
+        REQUIRE(window.getPointerToGLFWWindow());
+        REQUIRE(window.getName() == name);
+    }
+}
+
