9ea296488e41638425ecf6f64f36e3397c5e284b
Travis CI User <travis@example.org>
2021-04-24 05:02:30 +0000

    updating docs

--- menudata.js
+++ menudata.js
@@ -39,6 +39,7 @@ var menudata={children:[
 {text:"i",url:"functions_func_i.html#index_i"},
 {text:"m",url:"functions_func_m.html#index_m"},
 {text:"o",url:"functions_func_o.html#index_o"},
+{text:"p",url:"functions_func_p.html#index_p"},
 {text:"r",url:"functions_func_r.html#index_r"},
 {text:"s",url:"functions_func_s.html#index_s"},
 {text:"t",url:"functions_func_t.html#index_t"},
